b7a4b605f0632bcf15833657b3e585f9b02f0d22
Max Trussell <maxtrussell@gmail.com>
2020-06-30 23:52:17 -0600

    add user registration

--- app/models.py
+++ app/models.py
@@ -1,13 +1,27 @@
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import (
+    check_password_hash,
+    generate_password_hash,
+)
 
-from app import db
+from app import (
+    db,
+    login,
+)
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     events = db.relationship('Event', backref='owner', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -20,3 +34,7 @@ class Event(db.Model):
 
     def __repr__(self):
         return '<Event {}>'.format(self.description)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
